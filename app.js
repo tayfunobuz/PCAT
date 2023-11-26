b3256da6a67f7107d422413fcb1fdcd0eae9dfaa
Tayfun Obuz <obuztayfun@gmail.com>
2023-11-26 16:53:28 +0300

    Added static files.

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-const fn = function () {
-  console.log('code working')
-}
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+})
+
+const port = 3000
+
+app.listen(port)
